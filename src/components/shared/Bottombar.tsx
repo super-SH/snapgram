fdd9f40fd23d79d2c6544ca2881f4c43c41356ab
super-SH <dev.swam.htet@gmail.com>
2024-01-05 19:10:30 +0630

    feat: add mobile Bottombar

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,5 +1,23 @@
+import { bottombarLinks } from "@/constants";
+import { INavLink } from "@/types";
+import { NavLink } from "react-router-dom";
+
 function Bottombar() {
-  return <div>Bottombar</div>;
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link: INavLink) => (
+        <NavLink
+          key={`bottombar-${link.label}`}
+          to={link.route}
+          className="bottom-bar-link"
+        >
+          <img src={link.imgURL} alt={link.label} width={16} height={16} />
+
+          <p className="tiny-medium text-light-2">{link.label}</p>
+        </NavLink>
+      ))}
+    </section>
+  );
 }
 
 export default Bottombar;
