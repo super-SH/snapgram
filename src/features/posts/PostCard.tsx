ddf1fda8d092d177ec279b4ee53bb83e53ff1fb3
super-SH <dev.swam.htet@gmail.com>
2024-01-09 05:31:40 +0630

    feat:add post status

--- src/features/posts/PostCard.tsx
+++ src/features/posts/PostCard.tsx
@@ -17,8 +17,8 @@ function PostCard({ post }: PostCardProps) {
   console.log(post.creator);
 
   return (
-    <li className="post-card">
-      <div className="flex-between">
+    <li className="post-card flex flex-col gap-3">
+      <div className="flex-between ">
         <div className="flex items-center gap-3">
           <CreatorDetails
             creator={post.creator}
@@ -31,7 +31,7 @@ function PostCard({ post }: PostCardProps) {
       </div>
 
       <Link to={`posts/${post.id}`}>
-        <div className="small-medium lg:base-medium py-5">
+        <div className="small-medium lg:base-medium py-3">
           <p>{post.caption}</p>
           <ul className="mt-1 flex gap-1">
             {post.tags.map((tag) => (
@@ -47,6 +47,8 @@ function PostCard({ post }: PostCardProps) {
           alt={post.caption}
         />
       </Link>
+
+      <PostStats post={post} />
     </li>
   );
 }
@@ -102,6 +104,36 @@ function EditButton({
         width={20}
       />
     </Link>
+  );
+}
+
+function PostStats({ post }: { post: PostWithCreator }) {
+  const { data, isFetching } = useAccountInfo();
+  const accountId = data?.accountId;
+
+  return (
+    <div className="z-20 flex items-center justify-between">
+      <div className="flex gap-2">
+        <img
+          src={`/assets/icons/like.svg`}
+          alt={`outlined heart icon`}
+          width={20}
+          height={20}
+          className="cursor-pointer"
+        />
+        <p className="small-medium lg:base-medium">0</p>
+      </div>
+
+      <div>
+        <img
+          src={`/assets/icons/save.svg`}
+          alt={`outlined save file icon`}
+          width={20}
+          height={20}
+          className="cursor-pointer"
+        />
+      </div>
+    </div>
   );
 }
 
