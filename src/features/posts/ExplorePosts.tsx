625a1ffc43fdf40aa35077cfd8208b71eb202b7a
super-SH <dev.swam.htet@gmail.com>
2024-01-17 16:15:11 +0630

    feat: add Infinite scroll on explore page

--- src/features/posts/ExplorePosts.tsx
+++ src/features/posts/ExplorePosts.tsx
@@ -1,9 +1,21 @@
 import { flattenPagesData } from "@/lib/utils";
 import { useInfinitePosts } from "./useInfinitePosts";
 import GridPostList from "./GridPostList";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 function ExplorePosts() {
+  const { inView, ref } = useInView();
   const { data, fetchNextPage, hasNextPage } = useInfinitePosts();
+
+  useEffect(
+    function () {
+      if (inView) {
+        fetchNextPage();
+      }
+    },
+    [inView, fetchNextPage],
+  );
 
   // console.log(data);
   if (!data?.pages) return "loading";
@@ -18,10 +30,7 @@ function ExplorePosts() {
       <div className="flex-between mb-7 mt-16 w-full max-w-5xl">
         <h3 className="body-bold md:h3-bold">Popular Today</h3>
 
-        <div
-          className="flex-center cursor-pointer gap-3 rounded-xl bg-dark-3 px-4 py-2"
-          onClick={() => fetchNextPage()}
-        >
+        <div className="flex-center cursor-pointer gap-3 rounded-xl bg-dark-3 px-4 py-2">
           <p className="small-medium md:base-medium text-light-2">All</p>
           <img
             src="/assets/icons/filter.svg"
@@ -35,6 +44,12 @@ function ExplorePosts() {
       <div className="flex w-full max-w-5xl flex-wrap gap-9">
         <GridPostList posts={showExplorePosts} />
       </div>
+
+      {!hasNextPage ? null : (
+        <div ref={ref} className="mt-12">
+          Loading...
+        </div>
+      )}
     </>
   );
 }
