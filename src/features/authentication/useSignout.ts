0926e7da1c95fae8265a6eb3f617bac5d289ba8c
super-SH <dev.swam.htet@gmail.com>
2024-01-17 14:55:38 +0630

    fix: invalidateQuery after logout

--- src/features/authentication/useSignout.ts
+++ src/features/authentication/useSignout.ts
@@ -1,13 +1,19 @@
 import { signout as signoutApi } from "@/services/apiUser";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useSignout() {
+  const queryClient = useQueryClient();
+
   const {
     mutateAsync: signout,
     isPending,
     error,
   } = useMutation({
     mutationFn: signoutApi,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
   });
 
   return { signout, isPending, error };
