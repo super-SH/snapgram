97de1ba98175e721faa9bfcb053e8201ffa95339
super-SH <dev.swam.htet@gmail.com>
2024-01-20 03:58:24 +0630

    feat: add link in usercard

--- src/features/accounts/UserCard.tsx
+++ src/features/accounts/UserCard.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { AccountType } from "@/types/collection";
+import { Link } from "react-router-dom";
 
 type UserCardProps = {
   account: AccountType;
@@ -8,21 +9,25 @@ type UserCardProps = {
 
 function UserCard({ account, showFollowerCounts = false }: UserCardProps) {
   return (
-    <li className="user-card">
-      <img
-        src={"/assets/icons/profile-placeholder.svg"}
-        alt="default profile"
-      />
-      <div className="flex flex-col items-center justify-center gap-1">
-        <p className="small-medium text-light-2">{account.name}</p>
-        <p className="subtle-semibold text-light-4">
-          {showFollowerCounts ? "Followed by 0 users" : `@${account.username}`}
-        </p>
-      </div>
+    <li>
+      <Link to={`/profile/${account.id}`} className="user-card">
+        <img
+          src={"/assets/icons/profile-placeholder.svg"}
+          alt="default profile"
+        />
+        <div className="flex flex-col items-center justify-center gap-1">
+          <p className="small-medium text-light-2">{account.name}</p>
+          <p className="subtle-semibold text-light-4">
+            {showFollowerCounts
+              ? "Followed by 0 users"
+              : `@${account.username}`}
+          </p>
+        </div>
 
-      <Button className="shad-button_primary base-medium w-24 px-2 py-5">
-        Follow
-      </Button>
+        <Button className="shad-button_primary base-medium w-24 px-2 py-5">
+          Follow
+        </Button>
+      </Link>
     </li>
   );
 }
