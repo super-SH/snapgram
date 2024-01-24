3480d77fefbac168f33b29caf7e15a190745e9a0
super-SH <dev.swam.htet@gmail.com>
2024-01-24 21:23:03 +0630

    feat: invalidate all the data after signout

--- src/features/authentication/useSignout.ts
+++ src/features/authentication/useSignout.ts
@@ -12,7 +12,7 @@ export function useSignout() {
     mutationFn: signoutApi,
 
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.invalidateQueries();
     },
   });
 
