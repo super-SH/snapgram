bc73278bae861c1a716a6588af09c6102f562921
super-SH <dev.swam.htet@gmail.com>
2024-01-24 08:24:06 +0630

    fix: fix no top creators on page reload

--- src/features/accounts/TopCreators.tsx
+++ src/features/accounts/TopCreators.tsx
@@ -1,20 +1,30 @@
 import { useAccounts } from "@/features/accounts/useAccounts";
 import UserCardsContainer from "./UserCardsContainer";
 import { Loader } from "@/components/shared";
+import { useAccountInfo } from "./useAccountInfo";
 
 function TopCreators() {
-  // Change these data after implementing follow feature
+  // Need to call this hook because
+  const {} = useAccountInfo();
+  // accountId is undefined on initial render
+  // useAccounts will not trigger if there is no accountId
+
+  // so that , after calling this useAccountInfo , account data will get to the cache and accountId will no longer undefined
+
+  // This doesnt need for signin or signup , since account cache will set automatically
+  // But , need for cache are being clear by page reload
+
   const { data, isFetching } = useAccounts();
 
   if (isFetching)
-    <div className="flex-center h-full w-full">
-      <Loader />
-    </div>;
+    return (
+      <div className="flex-center h-full w-full">
+        <Loader />
+      </div>
+    );
 
   return (
-    <>
-      <UserCardsContainer accounts={data || []} showFollowerCounts />
-    </>
+    <>{data && <UserCardsContainer accounts={data} showFollowerCounts />}</>
   );
 }
 
