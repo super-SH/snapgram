fe5193f6e03bad63bb7bcbe6df6bf5f63ce8ffd3
super-SH <dev.swam.htet@gmail.com>
2024-01-26 19:19:20 +0630

    refactor: extract likebtn from poststats

---
+++ src/features/save-and-like-posts/LikeButton.tsx
@@ -0,0 +1,79 @@
+import { PostRecord } from "@/types/collection";
+import { useLikePost } from "./useLikePost";
+import { useLikedPostsRecord } from "./useLikedPostsRecord";
+import { useLikesCount } from "./useLikesCount";
+import { useUnlikePost } from "./useUnlikePost";
+import { formatCount } from "@/lib/utils";
+import { Loader } from "@/components/shared";
+
+interface PostLikeStatus {
+  isPostLiked: boolean;
+  likedPostRecordId?: number;
+}
+
+function getPostLikeStatus(
+  likedPostsRecord: PostRecord[] | undefined,
+  postId: number,
+): PostLikeStatus {
+  const likedPostRecordId = likedPostsRecord?.find(
+    (likedPost) => likedPost.postId === postId,
+  )?.id;
+
+  return {
+    isPostLiked: Boolean(likedPostRecordId),
+    likedPostRecordId,
+  };
+}
+
+type LikeBtnProps = {
+  postId: number;
+  accountId: number;
+};
+
+function LikeButton({ postId, accountId }: LikeBtnProps) {
+  const { data: likedPostsRecord } = useLikedPostsRecord();
+
+  const { likePost, isPending: isLikingPost } = useLikePost();
+  const { unlikePost, isPending: isUnlikingPost } = useUnlikePost();
+  const { data: likesCount } = useLikesCount(postId);
+
+  const { isPostLiked, likedPostRecordId } = getPostLikeStatus(
+    likedPostsRecord,
+    postId,
+  );
+
+  function handleLikePost(e: React.MouseEvent) {
+    e.stopPropagation();
+    if (!accountId) return;
+
+    if (!isPostLiked) {
+      likePost({ accountId, postId });
+    } else {
+      if (!likedPostRecordId) return;
+      unlikePost(likedPostRecordId);
+    }
+  }
+
+  if (isLikingPost || isUnlikingPost) return <Loader />;
+
+  return (
+    <div
+      className="flex h-5 items-center gap-2"
+      role="button"
+      onClick={handleLikePost}
+    >
+      <img
+        src={isPostLiked ? `/assets/icons/liked.svg` : `/assets/icons/like.svg`}
+        alt={isPostLiked ? `filled heart icon` : `outlined heart icon`}
+        width={20}
+        height={20}
+        className="cursor-pointer"
+      />
+      <p className="small-medium lg:base-medium">
+        {formatCount(likesCount || 0)}
+      </p>
+    </div>
+  );
+}
+
+export default LikeButton;
--- src/features/save-and-like-posts/PostStats.tsx
+++ src/features/save-and-like-posts/PostStats.tsx
@@ -1,131 +1,21 @@
-import React from "react";
-import { PostRecord, PostWithCreator } from "@/types/collection";
-import { useSavedPostsRecord } from "./useSavedPostsRecord";
-import { useSavePost } from "./useSavePost";
+import { PostWithCreator } from "@/types/collection";
 import { useAccountInfo } from "../accounts/useAccountInfo";
-import { useRemoveSavedPost } from "./useRemoveSavedPost";
-import { Loader } from "@/components/shared";
-import { useLikePost } from "./useLikePost";
-import { useUnlikePost } from "./useUnlikePost";
-import { useLikesCount } from "./useLikesCount";
-import { formatCount } from "@/lib/utils";
-import { useLikedPostsRecord } from "./useLikedPostsRecord";
+import SaveButton from "./SaveButton";
+import LikeButton from "./LikeButton";
 
 type PostStatsProps = { post: PostWithCreator };
 
-interface PostStatus {
-  isPostSaved: boolean;
-  isPostLiked: boolean;
-  savedPostRecordId?: number;
-  likedPostRecordId?: number;
-}
-
-function getPostStatus(
-  savedPostsRecord: PostRecord[] | undefined,
-  likedPostsRecord: PostRecord[] | undefined,
-  postId: number,
-): PostStatus {
-  const savedPostRecordId = savedPostsRecord?.find(
-    (savePost) => savePost.postId === postId,
-  )?.id;
-
-  const likedPostRecordId = likedPostsRecord?.find(
-    (likedPost) => likedPost.postId === postId,
-  )?.id;
-
-  return {
-    isPostSaved: Boolean(savedPostRecordId),
-    isPostLiked: Boolean(likedPostRecordId),
-    savedPostRecordId,
-    likedPostRecordId,
-  };
-}
-
 function PostStats({ post }: PostStatsProps) {
-  const { data: savedPostsRecord } = useSavedPostsRecord();
-  const { data: likedPostsRecord } = useLikedPostsRecord();
-
   const { data: accountData } = useAccountInfo();
-
-  const { savePost, isPending: isSavingPost } = useSavePost();
-  const { removeSavedPost, isPending: isRemovingSavedPost } =
-    useRemoveSavedPost();
-
-  const { likePost, isPending: isLikingPost } = useLikePost();
-  const { unlikePost, isPending: isUnlikingPost } = useUnlikePost();
-  const { data: likesCount } = useLikesCount(post.id);
-
   const accountId = accountData?.id;
-
-  const { isPostLiked, isPostSaved, savedPostRecordId, likedPostRecordId } =
-    getPostStatus(savedPostsRecord, likedPostsRecord, post.id);
-
-  function handleSavePost(e: React.MouseEvent) {
-    e.stopPropagation();
-    if (!accountId) return;
-
-    if (!isPostSaved) {
-      savePost({ accountId, postId: post.id });
-    } else {
-      if (!savedPostRecordId) return;
-      removeSavedPost(savedPostRecordId);
-    }
-  }
-
-  function handleLikePost(e: React.MouseEvent) {
-    e.stopPropagation();
-    if (!accountId) return;
-
-    if (!isPostLiked) {
-      likePost({ accountId, postId: post.id });
-    } else {
-      if (!likedPostRecordId) return;
-      unlikePost(likedPostRecordId);
-    }
-  }
 
   return (
     <div className="z-20 flex items-center justify-between gap-4">
-      <div className="flex gap-1" role="button" onClick={handleLikePost}>
-        {isLikingPost || isUnlikingPost ? (
-          <Loader />
-        ) : (
-          <div className="flex h-5 items-center gap-2">
-            <img
-              src={
-                isPostLiked
-                  ? `/assets/icons/liked.svg`
-                  : `/assets/icons/like.svg`
-              }
-              alt={isPostLiked ? `filled heart icon` : `outlined heart icon`}
-              width={20}
-              height={20}
-              className="cursor-pointer"
-            />
-            <p className="small-medium lg:base-medium">
-              {formatCount(likesCount || 0)}
-            </p>
-          </div>
-        )}
+      <div className="flex gap-1">
+        {accountId && <LikeButton postId={post.id} accountId={accountId} />}
       </div>
 
-      <div role="button" onClick={handleSavePost}>
-        {isSavingPost || isRemovingSavedPost ? (
-          <Loader />
-        ) : (
-          <img
-            src={
-              isPostSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"
-            }
-            alt={
-              isPostSaved ? "filled saved file icon" : "outlined save file icon"
-            }
-            width={20}
-            height={20}
-            className="cursor-pointer"
-          />
-        )}
-      </div>
+      {accountId && <SaveButton postId={post.id} accountId={accountId} />}
     </div>
   );
 }
